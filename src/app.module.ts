c1180c8a1f3a5e09e3a736915d61c435e49393e7
Abdullahi9 <abdullahi@nathandigital.com>
2025-02-20 16:28:04 +0300

    graphql bug fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { WebhookModule } from './webhook/webhook.module';
 
 @Module({
-  imports: [UsersModule, WebhookModule],
+  imports: [UsersModule, WebhookModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,12 +1,47 @@
-import { Resolver, Query, Args } from '@nestjs/graphql';
+import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
 import { UsersService } from './users.service';
+import { UserType } from './user.type';
 
-@Resolver('User')
+@Resolver(() => UserType)
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
 
-  @Query(() => [Object])
-  getUsers(@Args('limit', { type: () => Number, nullable: true }) limit?: number) {
-    return this.usersService.getUsers(limit);
+  // Query all users with pagination
+  @Query(() => [UserType])
+  getUsers(
+    @Args('limit', { type: () => Number, nullable: true }) limit?: number,
+    @Args('cursor', { type: () => String, nullable: true }) cursor?: string
+  ) {
+    return this.usersService.getUsers(limit, cursor);
+  }
+
+  // Query a single user
+  @Query(() => UserType)
+  getUserById(@Args('id', { type: () => String }) id: string) {
+    return this.usersService.getUserById(id);
+  }
+
+  // Mutation: Create a new user
+  @Mutation(() => UserType)
+  createUser(
+    @Args('name') name: string,
+    @Args('email') email: string,
+    @Args('phone') phone: string
+  ) {
+    return this.usersService.createUser({
+      name,
+      email,
+      phone
+    });
+  }
+
+  // Mutation: Update a user
+  @Mutation(() => UserType)
+  updateUser(
+    @Args('id') id: string,
+    @Args('name', { nullable: true }) name?: string,
+    @Args('phone', { nullable: true }) phone?: string
+  ) {
+    return this.usersService.updateUser(id, { name, phone });
   }
 }
